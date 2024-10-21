be97c6f88a60023b489c446b15a6541a0e55ca98
louis frederic schefer <lf.schefer@gmail.com>
2024-10-21 22:00:33 +0200

    update

--- sql-injection-ui/src/router.jsx
+++ sql-injection-ui/src/router.jsx
@@ -3,6 +3,7 @@ import Login from "./pages/Login";
 import Products from "./pages/Products";
 import ProductPage from "./pages/ProductPage";
 
+
 export const router = createBrowserRouter ([
   {
     path: "/",
